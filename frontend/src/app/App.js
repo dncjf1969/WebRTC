b48b5d9c3758a0e01673d837ba6c47ddbc62524b
chosohi <chosohi@naver.com>
2022-01-24 17:31:53 +0900

    CREATE: navbar&home 개발중

--- frontend/src/app/App.js
+++ frontend/src/app/App.js
@@ -1,11 +1,26 @@
 import React from "react";
-// import { BrowserRouter, Route } from "react-router-dom";
+import { BrowserRouter, Route } from "react-router-dom";
+import { StylesProvider } from "@material-ui/core/styles";
+// styled-components : UI 라이브러리
+import styled from "styled-components";
+import Home from "../features/home/Home";
 
+const Wrapper = styled.div`
+  background-color: rgba(246, 245, 253, 1);
+  height: 100vh;
+`;
 function App() {
   return (
-    <div>
-      <h1>Hello, world!</h1>
-    </div>
+    <StylesProvider injectFirst>
+      <Wrapper>
+        <BrowserRouter>
+          <Route path="/" exact component={Home} />
+          {/* <Route path="/signup" component={SignUp} /> */}
+          {/* <Route path="/tutorial" />
+          <Route path="/rank" /> */}
+        </BrowserRouter>
+      </Wrapper>
+    </StylesProvider>
   );
 }
 
--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,11 +1,23 @@
 import React from "react";
 import ReactDOM from "react-dom";
-import "./index.css";
-import App from "./App";
+// import { BrowserRouter } from "react-router-dom";
+import App from "./app/App.js";
+import { Provider } from "react-redux";
+// npm install redux-persist
+// Redux Persist를 사용하여 앱을 종료해도 지속되는 Store 만듦
+// 앱 종료 혹은 새로고침만 해도 저장되어있던 모든 state들이 없어진다.
+// 해당 라이브러리를 사용하여 마치 캐시 기능과 같이 상태값을 지속적으로 저장한다.
+import { PersistGate } from "redux-persist/integration/react";
+
+import configStore from "./app/store";
+
+const { store, persistor } = configStore();
 
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+  <Provider store={store}>
+    <PersistGate loading={null} persistor={persistor}>
+      <App />
+    </PersistGate>
+  </Provider>,
   document.getElementById("root")
 );
