32fc3e32b2ae3d695697c4bc612a544ad64a2afb
이정현 <simba155korea@gmail.com>
2022-01-24 20:33:35 +0900

    CREATE : 로그인 관련 DTO

---
+++ backend/src/main/java/com/wish/api/request/MemberLoginPostReq.java
@@ -0,0 +1,17 @@
+package com.wish.api.request;
+
+import io.swagger.annotations.ApiModel;
+import io.swagger.annotations.ApiModelProperty;
+import lombok.Getter;
+import lombok.Setter;
+
+
+@Getter
+@Setter
+@ApiModel("MemberLoginPostReq")
+public class MemberLoginPostReq {
+	@ApiModelProperty(name="유저 ID", example="ssafy_web")
+	String id;
+	@ApiModelProperty(name="유저 Password", example="your_password")
+	String password;
+}
