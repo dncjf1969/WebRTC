fb6502776babfc22a629b1f7f1f5dcc136a45280
이정현 <simba155korea@gmail.com>
2022-01-25 21:06:29 +0900

    CREATE : 회원 수정 기능

---
+++ backend/src/main/java/com/wish/api/request/MemberUpdateReq.java
@@ -0,0 +1,23 @@
+package com.wish.api.request;
+
+import io.swagger.annotations.ApiModel;
+import io.swagger.annotations.ApiModelProperty;
+import lombok.Getter;
+import lombok.Setter;
+
+
+@Getter
+@Setter
+@ApiModel("MemberUpdateReq")
+public class MemberUpdateReq {
+	@ApiModelProperty(name="아이디", example="ssafy_web")
+	String id;
+	@ApiModelProperty(name="패스워드", example="your_password")
+	String password;
+	
+	@ApiModelProperty(name="이름", example="홍길동")
+	String name;
+	
+	@ApiModelProperty(name="이메일", example="hong123@gmail.com")
+	String email;
+}
