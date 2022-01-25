b2a9d791ce654b73691b2957c63c36ca7382504b
chosohi <chosohi@naver.com>
2022-01-26 06:14:39 +0900

    UPDATE: 홈화면 유형별 버튼 생성

--- frontend/src/app/store.js
+++ frontend/src/app/store.js
@@ -9,7 +9,7 @@ import { combineReducers, configureStore } from "@reduxjs/toolkit";
 // import SignUpReducer from './features/SignupSlice';
 import SignUpReducer from "../features/account/authSlice";
 // reducer를 모아주는 함수,store를만들어주는 함수
-import AuthReducer from '../features/account/authSlice'
+import AuthReducer from "../features/account/authSlice";
 
 const rootreducer = combineReducers({
   // 각 리듀서를 합침
@@ -19,6 +19,10 @@ const rootreducer = combineReducers({
 
 const store = configureStore({
   reducer: rootreducer, // 합친 리듀서 연결
+  middleware: (getDefaultMiddleware) =>
+    getDefaultMiddleware({
+      serializableCheck: false,
+    }),
 });
 
 export default store; // 외부 인스톨이 가능하게 해줌
