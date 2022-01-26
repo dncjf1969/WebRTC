0ee3b1216b69b21a97c7a3b226431224d9127a11
조현아 <hyuna919@naver.com>
2022-01-26 23:11:39 +0900

    CREATE : 검색알고리즘 구현 위한 클래스 껍데기(내부 구조 미구현)

---
+++ backend/src/main/java/com/wish/common/util/SearchUtil.java
@@ -0,0 +1,18 @@
+package com.wish.common.util;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import com.wish.api.dto.WaitingRoom;
+import com.wish.api.dto.response.WaitingroomRes;
+
+public class SearchUtil {
+	
+	public List<WaitingroomRes> search(List<WaitingRoom> list, String keyword){
+		List<WaitingroomRes> res = new ArrayList<WaitingroomRes>();
+		
+		// map 순회하면서 조건에 맞는 방을 res에 추가해서 반환하기
+		
+		return res;
+	}
+}
