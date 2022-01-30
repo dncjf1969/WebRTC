1efbfb7bf161ea4f5e55e68ec84e3f45469693dd
이정현 <simba155korea@gmail.com>
2022-01-30 16:29:49 +0900

    UPDATE : 질문 관련 기능

--- backend/src/main/java/com/wish/api/service/QuestionServiceImpl.java
+++ backend/src/main/java/com/wish/api/service/QuestionServiceImpl.java
@@ -1,6 +1,9 @@
 package com.wish.api.service;
 
+import com.wish.api.dto.request.QuestionSelectReq;
+import com.wish.api.dto.request.RelationQuestionUpdateReq;
 import com.wish.db.entity.Question;
+import com.wish.db.entity.RelationQuestion;
 import com.wish.db.repository.QuestionRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
@@ -20,6 +23,23 @@ public class QuestionServiceImpl implements QuestionService {
 
         return questionList;
     }
+
+    public int selectedQuestionAddCnt1(QuestionSelectReq questionSelectReq){
+
+        Optional<Question> ques = questionRepository.findById(questionSelectReq.getQuestionId());
+
+        if(ques.isPresent()){
+            Question ques2 = ques.get();
+            int temp = ques2.getCount();
+            temp++;
+            ques2.setCount(temp);
+            questionRepository.save(ques2);
+
+            return 0;
+        }
+        else return 1;
+    }
+
 
 
 }
