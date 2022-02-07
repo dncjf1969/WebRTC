ce0b231f5ce9bc4bd9401fd1d81dfafb236a994b
장영남 <ggpp0909@naver.com>
2022-02-08 06:49:48 +0900

    vite 대문, 로그인, 회원가입디자인 수정,파비콘변경

---
+++ frontend/design/src/pages/Login.jsx
@@ -0,0 +1,102 @@
+import { Link } from 'react-router-dom';
+import React, { useState } from 'react';
+// import { useDispatch } from 'react-redux';
+import axios from '../common/http-common';
+import { saveToken } from '../common/JWT-common'
+import { useNavigate } from "react-router-dom";
+import Header from '../partials/Header';
+
+
+function Login() {
+
+   // state
+  const [ID, setID] = useState('');
+  const [password, setPassword] = useState('');
+
+  //navigate
+  const navigate = useNavigate()
+
+  // function
+  function handleSubmit(e) {
+    e.preventDefault();
+    const data = {
+      'id': ID,
+      'password': password,
+    };
+    login(data); // LoginSlice에서 가져온 로그인 액션, login(data) 는 createAsyncThunk로 만든것.
+  }
+  async function login (userInfo) {
+    try {
+      const response = await axios.post('/members/login', userInfo)
+      const {
+        data: {accessToken},
+      } = response;
+      saveToken(accessToken);
+      console.log(response)
+      navigate('/')
+      return response;
+    } catch (err) {
+      return(err.response)
+    }
+  };
+  return (
+    <div className="flex flex-col min-h-screen overflow-hidden">
+
+      {/*  Site header */}
+      <Header />
+
+      {/*  Page content */}
+      <main className="flex-grow">
+
+        <section className="bg-gradient-to-b from-gray-100 to-white">
+          <div className="max-w-6xl mx-auto px-4 sm:px-6">
+            <div className="pt-32 pb-12 md:pt-40 md:pb-20">
+
+              {/* Page header */}
+              <div className="max-w-3xl mx-auto text-center pb-12 md:pb-20">
+                <h1 className="h1">로그인해라 존말로할때</h1>
+              </div>
+
+              {/* Form */}
+              <div className="max-w-sm mx-auto">
+                <form onSubmit={handleSubmit}>
+                  <div className="flex flex-wrap -mx-3 mb-4">
+                    <div className="w-full px-3">
+                      <label className="block text-gray-800 text-sm font-medium mb-1" htmlFor="ID">아이디</label>
+                      <input id="ID" type="text" onChange={(e) => setID(e.target.value)} value={ID} autoFocus className="form-input w-full text-gray-800" placeholder="아이디를 입력해주세요." required />
+                    </div>
+                  </div>
+                  <div className="flex flex-wrap -mx-3 mb-4">
+                    <div className="w-full px-3">
+                      <div className="flex justify-between">
+                        <label className="block text-gray-800 text-sm font-medium mb-1" htmlFor="password">비밀번호</label>
+                        <Link to="reset-password" className="text-sm font-medium text-blue-600 hover:underline">Having trouble Loging in?</Link>
+                      </div>
+                      <input id="password" type="password" className="form-input w-full text-gray-800" onChange={(e) => setPassword(e.target.value)}
+            value={password} placeholder="비밀번호를 입력해주세요." required />
+                    </div>
+                  </div>
+                  
+                  <div className="flex flex-wrap -mx-3 mt-6">
+                    <div className="w-full px-3">
+                      <button className="btn text-white bg-blue-600 hover:bg-blue-700 w-full" >로그인</button>
+                    </div>
+                  </div>
+                </form>
+              
+                <div className="text-gray-600 text-center mt-6">
+                  WISH 회원이 아니신가요? <Link to="/signup" className="text-blue-600 hover:underline transition duration-150 ease-in-out">지금 가입하세요</Link>
+                </div>
+              </div>
+
+            </div>
+          </div>
+        </section>
+
+      </main>
+
+    </div>
+  );
+}
+
+export default Login;
