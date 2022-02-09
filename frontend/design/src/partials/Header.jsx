bc49b6de4ec174c81f3a3eedc4d23bc480f30690
chosohi <chosohi@naver.com>
2022-02-09 17:03:17 +0900

    design push

--- frontend/design/src/partials/Header.jsx
+++ frontend/design/src/partials/Header.jsx
@@ -44,7 +44,7 @@ function Header() {
               </li>
               :
               <li>
-                <Link to="/login" className=" btn-sm py-2 px-4 bg-blue-500 text-white font-semibold rounded-lg shadow-md hover:bg-blue-700 focus:outline-none focus:ring-2 focus:ring-blue-400 focus:ring-opacity-75 shadow hover:shadow-lg">로그인</Link>
+                <Link to="/login" className=" btn-sm py-2 px-4 bg-blue-500 text-white font-semibold rounded-lg shadow-md hover:bg-blue-700 focus:outline-none focus:ring-2 focus:ring-blue-400 focus:ring-opacity-75 shadow hover:shadow-lg ">로그인</Link>
               </li>
               }
               
