6abb925aaf87bfaf9a792058508f35828df52692
이정현 <simba155korea@gmail.com>
2022-02-09 11:38:56 +0900

    UPDATE : Redis 적용

--- backend/src/main/java/com/wish/api/service/RoomService.java
+++ backend/src/main/java/com/wish/api/service/RoomService.java
@@ -1,5 +1,11 @@
 package com.wish.api.service;
 
+import java.util.ArrayList;
+import java.util.Iterator;
+import java.util.List;
+import java.util.Set;
+import java.util.concurrent.TimeUnit;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.data.redis.core.ValueOperations;
@@ -7,8 +13,6 @@ import org.springframework.stereotype.Service;
 
 import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.JsonMappingException;
-import com.fasterxml.jackson.databind.JsonNode;
-import com.fasterxml.jackson.databind.ObjectMapper;
 import com.wish.api.dto.Room;
 
 
@@ -16,35 +20,61 @@ import com.wish.api.dto.Room;
 @Service
 public class RoomService {
 	
-	@Autowired
-	RedisTemplate<String, String> redisTemplate;
+	@Autowired 
+	RedisTemplate<String, Room> redisTemplate;
+
 	
 	//key value에 value에 해당하는 roomInfo 리턴해준다.
 	public Room getRoom(int roomId) throws JsonMappingException, JsonProcessingException {
+		ValueOperations<String, Room> roomValueOperaions = redisTemplate.opsForValue();
+		Room room = roomValueOperaions.get(Integer.toString(roomId));
 		
-		ValueOperations<String, String> vop = redisTemplate.opsForValue();
-		String roomInfo = vop.get(Integer.toString(roomId));
-		
-		ObjectMapper mapper =new ObjectMapper();
-		JsonNode node = mapper.readTree(roomInfo);
-		Integer memberCount = node.get("memberCount").asInt();
-		Integer memberMax = node.get("memberMax").asInt();
-		String password2 = node.get("password").asText();
-		String token = node.get("token").asText();
-		
-		Room room = new Room(token, roomId, node.get("manager").asText(), node.get("name").asText(), node.get("type").asText(), node.get("job").asText(), memberMax, memberCount, password2, node.get("nowMeeting").asBoolean());
 		return room;
 	}
 	
+	public List<Room> getRoomList(int roomType){ 
+		
+		Set<String> s = redisTemplate.keys("*");
+		Iterator<String> iter1 = s.iterator();
+		
+		List<Room> list1 = new ArrayList<Room>();
+		
+		String type = "";
+		if(roomType == 0) type = "인성";
+		else if(roomType == 1) type = "직무";
+		
+		while(iter1.hasNext()) {
+			String now = iter1.next();
+			if(now.contains("backup")) continue;
+			try {
+				Room room = getRoom( Integer.parseInt(now));
+				if(room.getType().equals(type)) list1.add(room);
+				
+			} catch (NumberFormatException | JsonProcessingException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+		}
+
+		return list1;
+	}
+//	
+//	// list (opsForList) 
+//	public void setRoomList(String key, Room room){ 
+//		redisTemplate.opsForList().rightPushAll(key, room);
+//	} 
+	
+//	public void setRoomList(String key, List<Room> values){ 
+//		redisTemplate.opsForList().rightPushAll(key, values);
+//	} 
+	
 	public void setRoom(Room room){
-		ValueOperations<String, String> vop2 = redisTemplate.opsForValue();
-		System.out.println("?");
-		//리스트에 다시 저장.
-		vop2.set(Integer.toString(room.getRoomId()), room.toString());
+		ValueOperations<String, Room> roomValueOperaions = redisTemplate.opsForValue();
+		roomValueOperaions.set(Integer.toString(room.getRoomId()), room);
+		redisTemplate.expire(Integer.toString(room.getRoomId()), 60, TimeUnit.MINUTES);
 	}
 	
 	public void deleteRoom(int roomId){
-		ValueOperations<String, String> vop2 = redisTemplate.opsForValue();
 		redisTemplate.delete(Integer.toString(roomId));
 	}
 	
