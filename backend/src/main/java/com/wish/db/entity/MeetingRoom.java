e489875376612751afd3ab7c57c3ac53eadf56e3
조현아 <hyuna919@naver.com>
2022-02-12 21:24:08 +0900

    CREATE : 면접 시작 + 종료시 mysql용 roomId로 면접 기록

--- backend/src/main/java/com/wish/db/entity/MeetingRoom.java
+++ backend/src/main/java/com/wish/db/entity/MeetingRoom.java
@@ -2,12 +2,14 @@ package com.wish.db.entity;
 
 import com.fasterxml.jackson.annotation.JsonIgnore;
 import com.fasterxml.jackson.annotation.JsonProperty;
+import com.wish.api.dto.Room;
 
 import lombok.Getter;
 import lombok.Setter;
 
 import java.sql.Date;
 import java.sql.Time;
+import java.text.SimpleDateFormat;
 
 import javax.persistence.Entity;
 
@@ -19,11 +21,25 @@ import javax.persistence.Entity;
 @Setter
 public class MeetingRoom extends BaseEntity{
 	// 고유id는 baseEntity에
-	// 면접타입, 전체인원, 면접 날짜, 면접 시작시간, 면접 걸린 시간
+	// 면접타입, 면접 세부분류, 전체인원, 면접 이름, 면접 날짜, 면접 시작시간, 면접 걸린 시간
 	String type;
-	int numberMember;
-	Date date;
-	Time startTime;
-	Time takenTime;
+	String job;
+//	int numberMember;
+	String name;
+	Long startTime;
+	Long takenTime;
+	
+	public static MeetingRoom of(Room room) {
+		MeetingRoom meetingRoom = new MeetingRoom();
+		meetingRoom.setType(room.getType());
+		meetingRoom.setJob(room.getJob());
+		meetingRoom.setName(room.getName());
+		
+		// 현재 시간 저장(시작시간)
+		Long startTime = System.currentTimeMillis();
+		meetingRoom.setStartTime(startTime);
+		
+		return meetingRoom;
+	}
     
 }
--- backend/src/main/java/com/wish/db/repository/MeetingRepository.java
+++ backend/src/main/java/com/wish/db/repository/MeetingRepository.java
@@ -15,6 +15,7 @@ import java.util.Optional;
  */
 @Repository
 public interface MeetingRepository extends JpaRepository<MeetingRoom, Long> {
-    // 아래와 같이, Query Method 인터페이스(반환값, 메소드명, 인자) 정의를 하면 자동으로 Query Method 구현됨.
     
+	// 면접 저장 및 id 반환
+	
 }
