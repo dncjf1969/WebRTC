ecc786ac1ec2d1a6741a054c1e768bb9707f2c38
조현아 <hyuna919@naver.com>
2022-02-18 12:16:12 +0900

    UPDATE : 메인페이지 영상 변경

--- frontend/src/partials/HeroHome.jsx
+++ frontend/src/partials/HeroHome.jsx
@@ -128,7 +128,7 @@ function HeroHome() {
                   <path d="M12 22c5.523 0 10-4.477 10-10S17.523 2 12 2 2 6.477 2 12s4.477 10 10 10zm0 2C5.373 24 0 18.627 0 12S5.373 0 12 0s12 5.373 12 12-5.373 12-12 12z" />
                   <path d="M10 17l6-5-6-5z" />
                 </svg>
-                <span className="ml-3">What is WISH? (2 min)</span>
+                <span className="ml-3">What is WISH? (4:40)</span>
               </button>
             </div>
             
@@ -140,7 +140,7 @@ function HeroHome() {
               handleClose={() => setVideoModalOpen(false)}
             >
               <div className="relative pb-9/16">
-                <iframe src="https://player.vimeo.com/video/674524235?h=741272f65a" width="640" height="360" frameborder="0" allow="autoplay; fullscreen; picture-in-picture" allowfullscreen></iframe>
+                <iframe src="https://player.vimeo.com/video/678978885?h=e9907617f5&amp;badge=0&amp;autopause=0&amp;player_id=0&amp;app_id=58479" style={{marginLeft: "250px"}} width="640" height="360" frameborder="0" allow="autoplay; fullscreen; picture-in-picture" allowfullscreen title="WISH.mp4"></iframe>
               </div>
             </Modal>
           </div>
